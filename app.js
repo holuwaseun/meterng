ac4621b4c88904ab9ed03d3316e273ffc76c81ca
holuwaseun <oluwaseun.olusi@gmail.com>
2017-02-13 17:30:45 +0100

    Initial commit -- MInc

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const morgan = require("morgan")
+
+let app = express()
+
+let http = require("http").Server(app)
+let io = require("socket.io")(http)
+
+app.use(morgan("dev"))
+
+app.use(express.static(`${ __dirname }/public`))
+app.use('/scripts', express.static(`${ __dirname }/node_modules`))
+
+app.get("*", (request, response) => {
+    response.sendFile(`${ __dirname }/public/app/views/index.html`)
+})
+
+http.listen(2285, (err) => {
+    if (err) {
+        console.log(err)
+    }
+})
