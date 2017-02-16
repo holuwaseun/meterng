f625bf0118dcbba27c14505b35673f3372adb7af
holuwaseun <oluwaseun.olusi@gmail.com>
2017-02-16 18:32:06 +0100

    Quick fix -- MInc

--- public/app/app.js
+++ public/app/app.js
@@ -24,6 +24,7 @@ angular.module("MeterNG", ["App-Routes", "Auth-Service", "Controller", "ngAnimat
 
         if ($rootScope.logged_in) {
             Auth.getUser().then((response) => {
+                console.log(response)
                 $rootScope.user_data = response.user_data
             })
         }
